cf26b5a3dbec782c9806c07d54a17da9a09b6521
Ferdinand Torggler <ferdinand.torggler@gmail.com>
2014-10-01 21:35:57 +0200

    Initial commit. Set up base structure

---
+++ Gulpfile.js
@@ -0,0 +1,109 @@
+var gulp = require('gulp');
+var es = require('event-stream');
+var gutil = require('gulp-util');
+var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
+var stylus = require('gulp-stylus');
+var cssmin = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var autoprefix = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var shell = require('gulp-shell');
+var jade = require('gulp-jade');
+var svgstore = require('gulp-svgstore');
+
+
+var styles = {
+    app: 'styles/main.styl',
+    watch: 'styles/*.styl',
+    vendor: 'styles/vendor/*.css',
+    dest: 'public/stylesheets'
+};
+
+var scripts = {
+    all: 'scripts/**/*.coffee',
+    vendor: 'scripts/vendor/**/*.js',
+    app: 'scripts/app/app.coffee',
+    dest: 'public/scripts/'
+};
+
+var templates = {
+    all: ['templates/angular/**/*.jade', 'index.jade'],
+    watch: ['templates/**/*.jade', 'index.jade'],
+    dest: 'public/'
+};
+
+var icons = {
+    all: 'icons/**/*.svg',
+    dest: 'public/icons/'
+};
+
+gulp.task('icons', function () {
+  var svgs = gulp.src(icons.all)
+            .pipe(svgstore({
+                prefix: 'icon-',
+                inlineSvg: true,
+                transformSvg: function (svg, cb) {
+                    svg.find('//*[@fill]').forEach(function (child) {
+                      child.attr('fill').remove();
+                    });
+                    svg.find('//*[@style]').forEach(function (child) {
+                        child.attr('style').remove()
+                    });
+                    cb(null);
+                }
+            }))
+            .pipe(gulp.dest(icons.dest))
+})
+
+gulp.task('styles', function () {
+    var app = gulp.src(styles.app).pipe(stylus()).on('error', gutil.log);
+    var vendor = gulp.src(styles.vendor);
+    return es.concat(vendor, app).on('error', gutil.log)
+        .pipe(concat('style.css'))
+        .pipe(cssmin())
+        .pipe(autoprefix('last 1 version'))
+        .pipe(gulp.dest(styles.dest));
+});
+
+gulp.task('scripts', function () {
+    gulp.src(scripts.vendor).pipe(gulp.dest(scripts.dest));
+    gulp.src(scripts.app, { read: false })
+      .pipe(browserify({
+        transform: ['coffeeify'],
+        extensions: ['.coffee']
+      })).on('error', gutil.log)
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest(scripts.dest));
+});
+
+gulp.task('templates', function () {
+    gulp.src(templates.all)
+      .pipe(jade({}))
+      .pipe(gulp.dest(templates.dest));
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'public/'
+    });
+});
+
+gulp.task('watch', function () {
+    var server = livereload();
+    gulp.watch(styles.watch, ['styles']);
+    gulp.watch(scripts.all, ['scripts']);
+    gulp.watch(templates.watch, ['templates']);
+    gulp.watch(icons.all, ['icons']);
+    gulp.watch('public/**').on('change', function (file) {
+        server.changed(file.path);
+    });
+})
+
+gulp.task('default', ['styles', 'scripts', 'templates', 'icons', 'connect', 'watch'], function () {
+});
+
+gulp.task('build', function () {
+    gulp.start('styles', 'scripts', 'templates', 'icons');
+});
