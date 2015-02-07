710a0ff9cf1b7d3c0bdea8fc73f6875127413abe
Ferdinand Torggler <ferdinand.torggler@gmail.com>
2015-02-07 19:11:05 +0100

    Add device type detection

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var device = require('express-device');
 
 var app = express();
 
@@ -13,10 +14,15 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(device.capture());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render('mobile/index');
+    if (req.device.type === 'desktop') {
+        res.send('hi desktops');
+    } else {
+        res.render('mobile/index');
+    }
 });
 
 http.createServer(app).listen(app.get('port'), function(){
